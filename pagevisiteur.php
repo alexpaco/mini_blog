f3c33dda706ee85157f7e82f637fac4ada0eb3e9
alexandre <alexandre.p@codeur.online>
2017-01-11 14:33:32 +0100

    Page visible par les visiteurs

---
+++ pagevisiteur.php
@@ -0,0 +1,14 @@
+<?php
+    
+?>
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Categorie, article</title>
+        <link rel="stylesheet" href="style.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <nav>Bonjour à toi simple visiteur</nav>
+    </body>
+</html>
