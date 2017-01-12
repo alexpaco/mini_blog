efbf488a795be77174d8e257f7ef56006eceb37e
alexandre <alexandre.p@codeur.online>
2017-01-12 13:47:40 +0100

    Envoi de données dans la table jointure

--- pageadmin.php
+++ pageadmin.php
@@ -14,27 +14,19 @@
             <a href="accueil.php">Se déconnecter</a>
         </nav>
         <form method="post" action="">
+            <textarea rows="5" cols="50" name="poste"></textarea>
             <select name="choix">
-                <option value="$choix">Animaux</option>
-                <option value="$choix">Jeux vidéo</option>
-                <option value="$choix">Voiture</option>
-                <option value="$choix">Sport</option>
+                <option value="1">Animaux</option>
+                <option value="2">Jeux vidéo</option>
+                <option value="3">Voiture</option>
+                <option value="4">Sport</option>
             </select>
-            <input type="submit" name="vers" value="Allez à"/>
+            <input type="submit" name="submit" value="poster"/>
         </form>
-    <?php
+        <?php
         include 'fonction.php';
+        entreeBDD();
         afficheArticle();
     ?>
-        <form method="post" action="">
-            <textarea rows="5" cols="50" name="poste"></textarea>
-            <input type="submit" name="submit" value="poster"/>
-            <select name="choix">
-                <option value="Animaux">Animaux</option>
-                <option value="Jeux vidéo">Jeux vidéo</option>
-                <option value=" Voiture">Voiture</option>
-                <option value="Sport">Sport</option>
-            </select>
-        </form>
     </body>
 </html>
