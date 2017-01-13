6d4d69c23f7e5b6506b04fd34ec0b6aff39b8622
alexandre <alexandre.p@codeur.online>
2017-01-13 15:47:19 +0100

    Fontion de deconnection active

--- pageadmin.php
+++ pageadmin.php
@@ -1,5 +1,16 @@
 <?php 
     session_start();
+    if(!isset($_SESSION['pseudo'])){
+        
+        die ("Vous êtes déconnecter");
+    }
+        if(isset($_POST['deco'])){
+                    
+            $_SESSION = array();
+            session_destroy();
+            header('Location: pagevisiteur.php');
+            exit();
+        }
 ?>
 <!DOCTYPE>
 <html>
@@ -9,24 +20,36 @@
         <meta charset="utf-8">
     </head>
     <body>
-        <nav>
+        <article>
+        <div id="poste">
             <p id="bonjour"><?php echo 'Bonjour, '.$_SESSION['pseudo'];?></p>
-            <a href="accueil.php">Se déconnecter</a>
-        </nav>
+                    
         <form method="post" action="">
-            <textarea rows="5" cols="50" name="poste"></textarea>
-            <select name="choix">
-                <option value="1">Animaux</option>
-                <option value="2">Jeux vidéo</option>
-                <option value="3">Voiture</option>
-                <option value="4">Sport</option>
+            <label for="titre">Titre : </label>
+            <input type="text" name="titre" id="titre"/></br></br>
+            <label for="poste">Texte : </label>
+            <textarea rows="5" cols="50" name="poste"></textarea></br></br>
+            <select name="choix" id="select">
+                <option>Choix catégorie</option>
+                <option value="1">Musique</option>
+                <option value="2">Concert</option>
+                <option value="3">Sport</option>
+                <option value="4">Jeux vidéo</option>
             </select>
             <input type="submit" name="submit" value="poster"/>
         </form>
-        <?php
-        include 'fonction.php';
-        entreeBDD();
-        afficheArticle();
-    ?>
+        <form method="post" action="">
+            <input type="submit" name="deco" value="se deconnecter"/>
+            
+        </form>    
+        </div>
+        <div class="ajout">
+            <?php
+                include 'fonction.php';
+                entreeBDD();
+                afficheArticle();
+            ?>
+        </div>
+        </article>
     </body>
 </html>
